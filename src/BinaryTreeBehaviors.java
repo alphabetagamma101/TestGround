8390e04571817ec6daa0166a3b810cc6d645ea71
Amit Manocha <amanoch@gap.com>
2020-04-02 21:25:56 -0700

    updated

--- src/BinaryTreeBehaviors.java
+++ src/BinaryTreeBehaviors.java
@@ -1,11 +1,14 @@
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class BinaryTreeBehaviors {
 
+    private TreePrinter treePrinter = new TreePrinter();
+
     public static void main(String[] args) {
         BinaryTreeBehaviors testClass = new BinaryTreeBehaviors();
+
+        // testClass.sampleTestTable();
+        // testClass.sampleTestTableWithSomeNullColumnData();
 
         testClass.findHeight();
         testClass.findDiameter();
@@ -15,11 +18,15 @@ public class BinaryTreeBehaviors {
 
     public void findHeight() {
         Node root = populateDataForBinaryTree();
+        treePrinter.printTree(root);
+
         System.out.println("Height: " + height(root));
     }
 
     public void findDiameter() {
         Node root = populateDataForBinaryTree();
+        treePrinter.printTree(root);
+
         System.out.println("Diameter: " + diameter(root));
     }
 
@@ -59,8 +66,8 @@ public class BinaryTreeBehaviors {
     // 3. leaf-nodes
     // see: https://www.youtube.com/watch?v=uemjIijtu2I
     private void printBoundaryTraversal() {
-        // TODO: prepare better binary tree as in video
-        Node node = populateDataForBinaryTree2();
+        Node node = prepareBinaryTree3();
+        treePrinter.printTree(node);
 
         // logic: add to set (no duplicates)
         Set<String> collector = new LinkedHashSet();
@@ -69,11 +76,14 @@ public class BinaryTreeBehaviors {
         collectRight(node, collector);
         collectLeaf(node, collector);
 
-        collector.stream().forEach(System.out::println);
+        System.out.println("Boundary traversal: ");
+        collector.stream().forEach(each -> System.out.print(each + " "));
+        System.out.println();
+        System.out.println(String.format("(total=%d)", collector.size()));
     }
 
     private void addToCollector(Set<String> collector, String data) {
-        System.out.println("   adding to collector: " + data);
+        // System.out.println("   adding to collector: " + data);
         collector.add(data);
     }
 
@@ -82,7 +92,7 @@ public class BinaryTreeBehaviors {
             return;
         }
 
-        System.out.println(" current: " + node.data);
+        // System.out.println(" current: " + node.data);
 
         if (null != node.left) {
             addToCollector(collector, node.data);
@@ -101,7 +111,7 @@ public class BinaryTreeBehaviors {
             return;
         }
 
-        System.out.println(" current2: " + node.data);
+        // System.out.println(" current2: " + node.data);
 
         if (null != node.right) {
             addToCollector(collector, node.data);
@@ -120,7 +130,7 @@ public class BinaryTreeBehaviors {
             return;
         }
 
-        System.out.println(" current3: " + node.data);
+        // System.out.println(" current3: " + node.data);
 
         if (null != node.left) {
             collectLeaf(node.left, collector);
@@ -146,6 +156,68 @@ public class BinaryTreeBehaviors {
 
     }
 
+    private Node prepareBinaryTree3() {
+        Node a = Node.builder()
+                .data("a")
+                .left(Node.builder()
+                        .data("b")
+                        .left(Node.builder()
+                                .data("c")
+                                .left(Node.builder()
+                                        .data("d")
+                                        .right(Node.builder()
+                                                .data("f")
+                                                .right(Node.builder()
+                                                        .data("e")
+                                                        .build())
+                                                .build())
+                                        .build())
+                                .right(Node.builder()
+                                        .data("g")
+                                        .build())
+                                .build())
+                        .right(Node.builder()
+                                .data("h")
+                                .build())
+                        .build())
+                .right(Node.builder()
+                        .data("k")
+                        .left(Node.builder()
+                                .data("j")
+                                .build())
+                        .right(Node.builder()
+                                .data("l")
+                                .left(Node.builder()
+                                        .data("m")
+                                        .build())
+                                .right(Node.builder()
+                                        .data("n")
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+
+        return a;
+    }
+
+    private Node prepareSmallestTree() {
+        Node node = Node.builder()
+                .data("a")
+                .left(Node.builder()
+                        .data("b")
+                        .left()
+                        .right()
+                        .build())
+                .right(Node.builder()
+                        .data("c")
+                        .left()
+                        .right()
+                        .build())
+                .build();
+
+        return node;
+    }
+
     private Node populateDataForBinaryTree() {
 
         Node a = populateDataForBinaryTree2();
@@ -153,7 +225,7 @@ public class BinaryTreeBehaviors {
         Node x = prepareNode("x");
         Node y = prepareNode("y", null, x);
 
-        Node z = prepareNode("Z", a, y);
+        Node z = prepareNode("z", a, y);
 
         return z;
     }
@@ -181,6 +253,8 @@ public class BinaryTreeBehaviors {
         return a;
     }
 
+
+
     private Node prepareNode(String data) {
         return new Node(data, null, null);
     }
@@ -189,7 +263,7 @@ public class BinaryTreeBehaviors {
         return new Node(data, left, right);
     }
 
-    class Node {
+    static class Node {
         String data;
         Node left;
         Node right;
@@ -201,7 +275,277 @@ public class BinaryTreeBehaviors {
         Node(String data) {
             this(data, null, null);
         }
+
+        public static NodeBuilder builder() {
+            return new NodeBuilder();
+        }
+
+        static class NodeBuilder {
+            private String data;
+            private Node left;
+            private Node right;
+
+            public Node build() {
+                return new Node(this.data, this.left, this.right);
+            }
+
+            public NodeBuilder data(String data) {
+                this.data = data;
+                return this;
+            }
+
+            public NodeBuilder left(Node left) {
+                this.left = left;
+                return this;
+            }
+
+            public NodeBuilder right(Node right) {
+                this.right = right;
+                return this;
+            }
+
+            public NodeBuilder left() {
+                this.left = null;
+                return this;
+            }
+
+            public NodeBuilder right() {
+                this.right = null;
+                return this;
+            }
+        }
     }
+
+    class TreePrinter {
+
+        private final String LEFT_SLASH = "/";
+        private final String RIGHT_SLASH = "\\";
+
+        public void printTree(Node node) {
+            printTree(node, 3);
+        }
+
+        public void printTree(Node node, int printColumnLength) {
+
+            TheTable theTable = new TheTable();
+            theTable.setPrintColumnLength(printColumnLength);
+
+            // using 15 column table
+            int middleColumn = 16;
+
+            addToTable(theTable, node, 0, middleColumn);
+
+            //System.out.println(String.format("rows=%d, columns=%d", theTable.rowSize(), theTable.rows().get(0).columnSize()));
+
+            theTable.print();
+        }
+
+        private void addToTable(TheTable theTable, Node node, int startRow, int middleColumn) {
+            if (null == node) {
+                return;
+            }
+            theTable.addData(String.format("(%s)", node.data), startRow, middleColumn);
+            if (null != node.left) {
+                // System.out.println("adding left: " + node.left.data);
+                theTable.addData(LEFT_SLASH, startRow+1, middleColumn-1);
+            }
+            if (null != node.right) {
+                // System.out.println("adding right: " + node.right.data);
+                theTable.addData(RIGHT_SLASH, startRow+1, middleColumn+1);
+            }
+
+            addToTable(theTable, node.left, startRow+2, middleColumn-2);
+            addToTable(theTable, node.right, startRow+2, middleColumn+2);
+        }
+
+    }
+
+    private void sampleTestTable() {
+        TheTable table = new TheTable();
+        int rows = 5;
+        int columns = 5;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                String value = String.format("[%d,%d]", i, j);
+                table.addData(value, i, j);
+            }
+        }
+
+        table.print();
+    }
+
+    private void sampleTestTableWithSomeNullColumnData() {
+        TheTable table = new TheTable();
+        int rows = 5;
+        int columns = 5;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (0 == (i+j) % 2) {
+                    String value = String.format("[%d,%d]", i, j);
+                    table.addData(value, i, j);
+                }
+            }
+        }
+
+        table.print();
+    }
+
+
+    // for pretty print above binary tree
+    class TheTable {
+
+        private Map<Integer, Row> container = new TreeMap();
+
+        private int printColumnLength = 10;
+
+        private Utils utils = new Utils();
+
+        public Map<Integer, Row> rows() {
+            return container;
+        }
+
+        public int rowSize() {
+            return utils.getSize(container);
+        }
+
+        public void addData(String data, int rowNum, int columnNum) {
+            Row row = container.get(rowNum);
+            if (null == row) {
+                container.put(rowNum, new Row());
+                row = container.get(rowNum);
+            }
+
+            Row.Column column = row.getColumn(columnNum);
+
+            // to avoid overlaps for tree nodes
+            if (null == column.data) {
+                column.setData(data);
+            } else {
+                column.setData(column.data + "" + data);
+            }
+        }
+
+        public void setPrintColumnLength(int printColumnLength) {
+            this.printColumnLength = printColumnLength;
+        }
+
+        public int getPrintColumnLength() {
+            return printColumnLength;
+        }
+
+        public void print() {
+            System.out.println();
+            System.out.println();
+            System.out.println(getPrintData());
+            System.out.println();
+            System.out.println();
+        }
+
+        public String getPrintData() {
+            final StringBuilder stringBuilder = new StringBuilder();
+            final int rowSize = rowSize();
+            for (int i = 0; i < rowSize; i++) {
+                final Row row = container.get(i);
+                if (null == row) {
+                    // System.out.println("print: null row:i="+i);
+                    stringBuilder.append(Utils.NEWLINE);
+                    continue;
+                }
+
+                final int columnSize = row.columnSize();
+                for (int j = 0; j < columnSize; j++) {
+                    final Row.Column column = row.getColumn(j);
+                    if (null == column || null == column.data) {
+                        // System.out.println("print: null column/its data: j="+j);
+                        stringBuilder.append(utils.preparePrintData(""));
+                    } else {
+                        stringBuilder.append(utils.preparePrintData(column.getData()));
+                    }
+                }
+
+                stringBuilder.append(Utils.NEWLINE);
+            }
+
+            return stringBuilder.toString();
+        }
+
+        class Utils {
+
+            private static final String NEWLINE = "\n";
+
+            public <T> int getSize(Map<Integer, T> container) {
+                final Set<Integer> keys = container.keySet();
+
+                // get last element for number of rows
+                final Iterator<Integer> iterator = keys.iterator();
+                Integer next = new Integer(-1);
+                while(iterator.hasNext()) {
+                    next = iterator.next();
+                }
+                return 1 + next;
+            }
+
+            public String preparePrintData(String data) {
+                // TODO: better?
+                int printColumnLenBy2 = getPrintColumnLength()/2;
+                int halfLen = data.length()/2;
+                final String gapEachSide = getGap(printColumnLenBy2 - halfLen);
+                return gapEachSide + data + gapEachSide;
+            }
+
+            private String getGap(int n) {
+                String gap = " ";
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i=0; i<n; i++) {
+                    stringBuilder.append(gap);
+                }
+                return stringBuilder.toString();
+            }
+        }
+
+        class Row {
+
+            private Map<Integer, Column> container = new TreeMap();
+
+            public Map<Integer, Column> columns() {
+                return container;
+            }
+
+            public int columnSize() {
+                return utils.getSize(container);
+            }
+
+            public Column getColumn(int columnNum) {
+                Column column = container.get(columnNum);
+                if (null == column) {
+                    container.put(columnNum, new Column());
+                    column = container.get(columnNum);
+                }
+                return column;
+            }
+
+            class Column {
+
+                private String data;
+
+                public Column() {
+                }
+
+                public Column(String data) {
+                    this.data = data;
+                }
+
+                public void setData(String data) {
+                    this.data = data;
+                }
+
+                public String getData() {
+                    return data;
+                }
+            }
+        }
+    }
+
 }
 
 /**
@@ -223,3 +567,4 @@ public class BinaryTreeBehaviors {
 
 
  */
+
