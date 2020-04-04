122bdca6e480f93e043ed21f82e4478a1e870f5a
Amit Manocha <amanoch@gap.com>
2020-04-03 18:31:29 -0700

    updated

--- src/Merge3SortedLinkedLists.java
+++ src/Merge3SortedLinkedLists.java
@@ -1,11 +1,12 @@
-import java.util.Objects;
+import java.util.*;
 
-// Uses inline without extra space
 public class Merge3SortedLinkedLists {
 
     private Node nodeA;
     private Node nodeB;
     private Node nodeC;
+
+    private final Map<Integer, Node> ALL_LINKED_LISTS = new HashMap<>();
 
      public static void main(String[] args) {
         Merge3SortedLinkedLists testClass = new Merge3SortedLinkedLists();
@@ -19,6 +20,10 @@ public class Merge3SortedLinkedLists {
 
     public Node execute() {
         populateData();
+
+        ALL_LINKED_LISTS.put(0, nodeA);
+        ALL_LINKED_LISTS.put(1, nodeB);
+        ALL_LINKED_LISTS.put(2, nodeC);
 
         Node result = getMinimum();
 
@@ -34,46 +39,52 @@ public class Merge3SortedLinkedLists {
     }
 
     private Node getMinimum() {
-        Node tmp = null;
+         return getMinimumNew();
+    }
 
-        // TODO: optimize by putting in 1 list and
-        //  provide 'starting offset' for remaining nodes to find mimimum
-
-        if (null != nodeA) {
-            tmp = nodeA;
-            if (null != nodeB && tmp.data > nodeB.data) {
-                tmp = nodeB;
-            }
-            if (null != nodeC && tmp.data > nodeC.data) {
-                tmp = nodeC;
-            }
-        } else if (null != nodeB) {
-            tmp = nodeB;
-            if (null != nodeC && tmp.data > nodeC.data) {
-                tmp = nodeC;
-            }
-        } else if (null != nodeC) {
-            tmp = nodeC;
-        }
-
+    private Node getMinimumNew() {
+        final Node tmp = getNodeWithSmallestValue(0);
         if (null == tmp ) {
             // terminating condition
             return null;
         }
+        moveMatchingNodeToNextElement(tmp);
+        return tmp;
+    }
 
-        if (tmp.equals(nodeA)) {
-            nodeA = nodeA.next;
-        }
-        if (tmp.equals(nodeB)) {
-            nodeB = nodeB.next;
-        }
-        if (tmp.equals(nodeC)) {
-            nodeC = nodeC.next;
-        }
+    private Node getNodeWithSmallestValue(final int startIndex) {
+        Node tmp;
 
-        System.out.println(tmp.data);
+        if (null != ALL_LINKED_LISTS.get(startIndex)) {
+            tmp = ALL_LINKED_LISTS.get(startIndex);
+            // System.out.println(tmp);
+            // iterate on remaining to 'keep' the smallest
+            for (int i = startIndex + 1; i < ALL_LINKED_LISTS.size(); i++) {
+                Node next = ALL_LINKED_LISTS.get(i);
+                if (null != next && tmp.data > next.data) {
+                    tmp = next;
+                }
+            }
+        } else {
+            // node at startIndex is null
+            if (0 == startIndex) {
+                return null;
+            }
+            return getNodeWithSmallestValue(startIndex - 1);
+        }
 
         return tmp;
+    }
+
+    private void moveMatchingNodeToNextElement(Node tmp) {
+        for (int i = 0; i < ALL_LINKED_LISTS.size();i++) {
+            Node next = ALL_LINKED_LISTS.get(i);
+            if (tmp.equals(next)) {
+                next = next.next;
+                ALL_LINKED_LISTS.put(i, next);
+                break;
+            }
+        }
     }
 
     private void populateData() {
@@ -145,5 +156,61 @@ public class Merge3SortedLinkedLists {
         public int hashCode() {
             return Objects.hash(data, next);
         }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    '}';
+        }
+    }
+
+    // ===== older non-optimized solutions =====
+
+
+    private Node getMinimumOld() {
+        Node tmp = null;
+
+        // TODO: optimize by putting in 1 list and
+        //  provide 'starting offset' for remaining nodes to find mimimum
+        // note: see new getMinimum method above
+
+        if (null != nodeA) {
+            tmp = nodeA;
+            if (null != nodeB && tmp.data > nodeB.data) {
+                tmp = nodeB;
+            }
+            if (null != nodeC && tmp.data > nodeC.data) {
+                tmp = nodeC;
+            }
+        } else if (null != nodeB) {
+            tmp = nodeB;
+            if (null != nodeC && tmp.data > nodeC.data) {
+                tmp = nodeC;
+            }
+        } else if (null != nodeC) {
+            tmp = nodeC;
+        }
+
+        // System.out.println(tmp);
+
+        if (null == tmp ) {
+            // terminating condition
+            return null;
+        }
+
+        if (tmp.equals(nodeA)) {
+            nodeA = nodeA.next;
+        }
+        if (tmp.equals(nodeB)) {
+            nodeB = nodeB.next;
+        }
+        if (tmp.equals(nodeC)) {
+            nodeC = nodeC.next;
+        }
+
+        System.out.println(tmp.data);
+
+        return tmp;
     }
 }
